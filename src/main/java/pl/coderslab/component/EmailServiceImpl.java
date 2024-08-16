4d67509d0d5948b1bd8e2bc5f4cc8c9fa800f8eb
Anastazja-ch <chteinmananastazja@gmail.com>
2024-08-16 11:57:51 +0200

    changes

--- src/main/java/pl/coderslab/component/EmailServiceImpl.java
+++ src/main/java/pl/coderslab/component/EmailServiceImpl.java
@@ -15,9 +15,9 @@ public class EmailServiceImpl {
             String from, String subject, String text) {
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("anastazjachteinman@gmail.com"); // adres e-mail osoby wysyłającej
-        message.setTo("testowankomjupku@gmail.com"); // Twój adres e-mail, na który mają być wysyłane wiadomości
-        message.setText(text); // Treść wiadomości
+        message.setFrom(""); // adres e-mail osoby wysyłającej
+        message.setTo("testowankomjupku@gmail.com");
+        message.setText(text);
         emailSender.send(message);
 
     }
--- src/main/java/pl/coderslab/config/MailConfig.java
+++ src/main/java/pl/coderslab/config/MailConfig.java
@@ -13,17 +13,17 @@ public class MailConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("sandbox.smtp.mailtrap.io"); // Zmiana hosta na Mailtrap
+        mailSender.setHost("sandbox.smtp.mailtrap.io");
         mailSender.setPort(587); // Port Mailtrap
 
-        mailSender.setUsername("641c22232c7fa0"); // Username Mailtrap
-        mailSender.setPassword("43005ecc00e949"); // Hasło Mailtrap (upewnij się, że jest poprawne)
+        mailSender.setUsername("641c22232c7fa0");
+        mailSender.setPassword("43005ecc00e949"); // Hasło Mailtrap
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io"); // Zmiana trust na Mailtrap
+        props.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io");
         props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.debug", "true");
 
